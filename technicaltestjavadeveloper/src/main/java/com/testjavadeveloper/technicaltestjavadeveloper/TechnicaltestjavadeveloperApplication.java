36b1c6099db41a65d619eabaa21e8136ac5c245d
Fikri Zulfikar Kusmana <57424206+croxe325@users.noreply.github.com>
2022-10-20 19:37:41 +0700

    Add files via upload

---
+++ technicaltestjavadeveloper/src/main/java/com/testjavadeveloper/technicaltestjavadeveloper/TechnicaltestjavadeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.testjavadeveloper.technicaltestjavadeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicaltestjavadeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicaltestjavadeveloperApplication.class, args);
+	}
+
+}
---
+++ technicaltestjavadeveloper/src/test/java/com/testjavadeveloper/technicaltestjavadeveloper/TechnicaltestjavadeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testjavadeveloper.technicaltestjavadeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicaltestjavadeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
